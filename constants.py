d3b9dbeeb141441336ef2b10e94524824b5d4954
IvanovArtem123 <akk0nom1@gmail.com>
2026-04-12 21:16:42 +0300

    Remove constants

--- constants.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-BASE_URL = os.getenv('BASE_URL', '')
-LOGIN_URL = BASE_URL + "/login"
-BASE_URL_INBOUNDS = os.getenv('BASE_URL_INBOUNDS', '')
--- schemas/user.py
+++
@@ -1,8 +0,0 @@
-from enum import IntEnum
-
-
-class UserRole(IntEnum):
-    """Роль пользователя."""
-
-    USER = 0
-    ADMIN = 1
